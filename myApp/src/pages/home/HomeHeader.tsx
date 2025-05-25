d2ec256f4049c7b2ab191fed4a6d8f9db16506ab
Nguyen Van A <nguyenvana@gmail.com>
2025-05-25 11:11:53 +0700

    Add all missing components and update existing files

--- myApp/src/pages/home/HomeHeader.tsx
+++ myApp/src/pages/home/HomeHeader.tsx
@@ -1,25 +1,61 @@
+import React from "react";
 import {
   IonHeader,
   IonToolbar,
-  IonTitle,
+  IonButtons,
   IonButton,
-} from '@ionic/react';
-import { useHistory } from 'react-router';
+  IonTitle,
+  IonImg,
+  IonSegment,
+  IonSegmentButton,
+  IonLabel,
+} from "@ionic/react";
 
-const HomeHeader: React.FC = () => {
-  const history = useHistory();
+import group8946 from "./group-8946.png";
+import group8981 from "./group-8981.png";
+import line30 from "./line-30.svg";
+import vector166 from "./vector-166.svg";
+
+import "./style.css";
+
+export const Header: React.FC = () => {
   return (
     <IonHeader>
-      <IonToolbar>
-        <IonTitle>Home đã đăng nhập</IonTitle>
-        <IonButton         
-          onClick={() => history.push('/homepage')}
-        >
-          Đăng xuất
-        </IonButton>
+      <IonToolbar className="header-toolbar">
+        {/* Logo bên trái */}
+        <IonImg src={group8946} alt="Logo" className="logo" />
+
+        {/* Các nút Sign In / Sign Up bên phải */}
+        <IonButtons slot="end" className="auth-buttons">
+          <IonButton>
+            Sign In
+          </IonButton>
+          <IonButton fill="outline">
+            Sign Up
+          </IonButton>
+        </IonButtons>
+      </IonToolbar>
+
+      {/* Thanh menu chính dưới toolbar */}
+      <IonToolbar className="menu-toolbar">
+        <IonSegment scrollable>
+          <IonSegmentButton value="home">
+            <IonLabel>Trang chủ</IonLabel>
+          </IonSegmentButton>
+          <IonSegmentButton value="tour">
+            <IonLabel>Tour</IonLabel>
+          </IonSegmentButton>
+          <IonSegmentButton value="place">
+            <IonLabel>Địa điểm</IonLabel>
+          </IonSegmentButton>
+          <IonSegmentButton value="hotel">
+            <IonLabel>Khách sạn</IonLabel>
+          </IonSegmentButton>
+        </IonSegment>
+
+        {/* Ảnh hoặc icon trang trí */}
+        <IonImg src={group8981} alt="Decoration" className="decoration-img" />
       </IonToolbar>
     </IonHeader>
   );
 };
-
-export default HomeHeader;
