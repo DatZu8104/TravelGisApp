6dd7ad54a045061832a6462264b6620e5e6259c0
Nguyen Van A <nguyenvana@gmail.com>
2025-05-30 13:21:33 +0700

    Update mới

--- myApp/src/App.tsx
+++ myApp/src/App.tsx
@@ -49,7 +49,7 @@ const App: React.FC = () => (
           />
         ))}
         <Route exact path="/">
-          <Redirect to="/homemain" />
+          <Redirect to="/homepage" />
         </Route>
       </IonRouterOutlet>
     </IonReactRouter>
--- myApp/src/components/HeroSection/HeroSection.tsx
+++ myApp/src/components/HeroSection/HeroSection.tsx
@@ -1,66 +1,16 @@
-import { IonGrid, IonRow, IonCol, IonImg } from '@ionic/react';
+import React from 'react';
 import './HeroSection.css';
 
-// Đường dẫn hình ảnh từ assets
-import backgroundImage from './assets/nen-bien.png';
-import circle1 from './assets/tron-1.png';
-import circle2 from './assets/tron-2.png';
-import circle3 from './assets/tron-3.png';
-import weNeedImage from './assets/we-need.png';
-import marineVitaminImage from './assets/marine-vitamin.png';
+import bannerImage from './assets/anh-1.png';
+import belowImage from './assets/anh-2.png';
 
-// Góc sóng
-import vangTrai from './assets/vang-trai.png';
-import xanhTrai from './assets/xanh-trai.png';
-import vangPhai from './assets/vang-phai.png';
-import xanhPhai from './assets/xanh-phai.png';
-
-const HeroSection = () => {
+const HeroSection: React.FC = () => {
   return (
-    <div
-      className="hero-section"
-      style={{ backgroundImage: `url(${backgroundImage})` }}
-    >
-      <div className="hero-overlay">
-        {/* Tiêu đề */}
-        <IonImg src={weNeedImage} alt="WE NEED" className="hero-title-img" />
-        <IonImg
-          src={marineVitaminImage}
-          alt="Marine Vitamin"
-          className="hero-subtitle-img"
-        />
+    <div className="hero-section">
+      <img src={bannerImage} alt="Marine Banner" className="main-banner" />
 
-        {/* Góc sóng */}
-        <div className="corner-waves">
-          <img src={vangTrai} alt="Vàng trái" className="wave vang-trai" />
-          <img src={xanhTrai} alt="Xanh trái" className="wave xanh-trai" />
-          <img src={vangPhai} alt="Vàng phải" className="wave vang-phai" />
-          <img src={xanhPhai} alt="Xanh phải" className="wave xanh-phai" />
-        </div>
-
-        {/* Hình tròn nổi có bóng */}
-        <IonGrid className="hero-circles">
-          <IonRow className="ion-justify-content-center ion-align-items-center">
-            <IonCol size="12" size-md="4">
-              <div className="circle-wrapper circle-1">
-                <div className="circle-shadow"></div>
-                <IonImg src={circle1} alt="Ảnh tròn 1" />
-              </div>
-            </IonCol>
-            <IonCol size="12" size-md="4">
-              <div className="circle-wrapper circle-2">
-                <div className="circle-shadow"></div>
-                <IonImg src={circle2} alt="Ảnh tròn 2" />
-              </div>
-            </IonCol>
-            <IonCol size="12" size-md="4">
-              <div className="circle-wrapper circle-3">
-                <div className="circle-shadow"></div>
-                <IonImg src={circle3} alt="Ảnh tròn 3" />
-              </div>
-            </IonCol>
-          </IonRow>
-        </IonGrid>
+      <div className="below-image-wrapper">
+        <img src={belowImage} alt="Below Banner" className="below-image" />
       </div>
     </div>
   );
